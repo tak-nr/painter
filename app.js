bf5230f636affa07b96ed1f12fd3b13ccafdd9ca
tak n.r <taknarae0620@gmail.com>
2021-02-20 03:05:06 +0900

    캔버스 사이즈 변경 기능 추가

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 let painting = false;
 let colors = document.getElementsByClassName('color');
+let sizeChangeBtn = document.getElementById('sizeChangeBtn');
+
 context.strokeStyle = '#000000';
 
 function startPainting(){
@@ -31,6 +33,23 @@ function onColorSelect(event){
     context.strokeStyle = color.style.backgroundColor;
 }
 
+function onCanSizeChange(event){
+    const canWidth = document.getElementById('canWidth');
+    const canHeight = document.getElementById('canHeight');
+    
+    if(Number.parseInt(canWidth.value) > Number.parseInt(canWidth.max)){
+        canWidth.value = canWidth.max;
+    }
+    if(Number.parseInt(canHeight.value) > Number.parseInt(canHeight.max)){
+        canHeight.value = canHeight.max;
+    }
+
+    canvas.style.width = canWidth.value + 'px';
+    canvas.style.height = canHeight.value + 'px';
+    canvas.width = canWidth.value;
+    canvas.height = canHeight.value;
+}
+
 
 if(canvas){
     canvas.addEventListener('mousemove', onMouseMove);
@@ -41,4 +60,8 @@ if(canvas){
 
 if(colors){
     Array.from(colors).forEach(color => color.addEventListener('mousedown', onColorSelect));
+}
+
+if(sizeChangeBtn){
+    sizeChangeBtn.addEventListener('click', onCanSizeChange);
 }
